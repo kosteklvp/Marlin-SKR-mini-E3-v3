c278c5d06b4acc67b2a7542f4008009c303ac90b
kosteklvp <kostek.lvp@gmail.com>
2023-04-13 18:04:55 +0200

    Update Configuration.h
    changed bed size to 233x233

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1381,8 +1381,8 @@
 // @section machine
 
 // The size of the printable area
-#define X_BED_SIZE 230
-#define Y_BED_SIZE 230
+#define X_BED_SIZE 233
+#define Y_BED_SIZE 233
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
